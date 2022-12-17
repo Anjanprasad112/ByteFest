0bbd8995cfbde914bda4bcce3c402983ba6a7bab
Anjan Prasad <anjanprasad112@gmail.com>
2022-12-17 23:28:02 +0530

    5/5

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,7 +5,7 @@ import imgthree from '../assets/ticket.png';
 
 const Stats = () => {
   return (
-    <div className="md:p-10 flex flex-row justify-between items-center w-full z-2 bg-white h-40">
+    <div className="md:p-10 flex flex-row justify-between justify-items-center items-center w-full z-2 bg-white h-40">
         <div className=""><div className="flex flex-col justify-center items-center"><img src={imgone} alt="calender" className="h-12 md:h-20 m-2 " /> <p className="m-1"><p>21st & 23rd</p><p>December</p> </p></div> 
         </div>
         <div ><div className="flex flex-col justify-center items-center"><img src={imgtwo} alt="location" className="h-12 md:h-20 m-2 " /> <p className="m-1 text-center">GAT Campus</p></div> </div>
