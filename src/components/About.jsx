49cbdf9ca218b7d19a6147873376c6cf98102213
Anjan Prasad <anjanprasad112@gmail.com>
2022-12-19 10:58:16 +0530

    13/13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
     <div>
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-center mt-4">ABOUT</h1>
-          <div className="text-center text-[15px] md:text-xl p-5">
+          <div className="text-center text-sm md:text-xl p-5">
             <p>
 Bytefest - 2022 is the right opportunity to showcase your coding abilities and nurture your interest as a coder. <br/>
 Bytefest – 2022 will not only provide the new batch of students a practical approach to problem solving, experimenting and brainstorming, but will also encourage them to put forth their innovative ideas as versatile code. <br/>
