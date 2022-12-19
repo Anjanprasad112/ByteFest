4e4247fec7bdfc30912c0a383e1ede16a8ea9cca
Anjan Prasad <anjanprasad112@gmail.com>
2022-12-19 10:43:28 +0530

    9/9

--- src/Anno.jsx
+++ src/Anno.jsx
@@ -12,7 +12,7 @@ const Anno = () => {
     <div>
         <div className="flex flex-col justify-center items-center text-center">
           <h1 className="m-5">ANNOUNCEMENTS</h1>
-          <p className="m-5">Stay Tuned For More Updates</p>
+          <p className="m-5 text-xl">Stay Tuned For More Updates</p>
         <button className=" text-white m-5"
             onClick={routeChange}
               >
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,8 +18,8 @@ const Contact = () => {
     <div>
         <div className="h-96 flex flex-col items-center justify-center"><h1 className="text-center m-2">Contact Us</h1>
         <div className="">
-            <p className="m-4">Anoop R - <span><button onClick={callone} className="text-white">Call</button></span></p>
-            <p className="m-4">IMPANA -  <span> <button onClick={calltwo} className="text-white">Call</button></span></p>
+            <p className="m-4 text-xl">Anoop R - <span><button onClick={callone} className="text-white">Call</button></span></p>
+            <p className="m-4 text-xl">IMPANA -  <span> <button onClick={calltwo} className="text-white">Call</button></span></p>
         </div>
          <button className="m-auto p-auto text-white "
             onClick={routeChange}
