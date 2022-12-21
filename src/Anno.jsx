9e6df9f9cd07eca9dc468b9160d41d27458bccd3
Anjan Prasad <anjanprasad112@gmail.com>
2022-12-21 07:45:13 +0530

    26/26

--- src/Anno.jsx
+++ src/Anno.jsx
@@ -13,7 +13,7 @@ const Anno = () => {
         <div className="flex flex-col justify-center items-center text-center">
           <h1 className="m-5">ANNOUNCEMENTS</h1>
           <div className="m-5 text-xl">
-            <p className='text-xl m-2 font-medium'>First Round Starts at 9:30 AM Tommorrow</p> <br/>
+            <p className='text-xl m-2 font-medium'>First Round Starts at 9:30 AM Today (21/12/22)</p> <br/>
             <p className='text-xl m-2 font-medium'>Offline Registrations at the Auditorium from 8:30 AM to 9:30 AM</p>
           </div>
         <button className=" text-white m-5"
